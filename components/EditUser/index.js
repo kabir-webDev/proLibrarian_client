1e92010a427ef00705fa51af750b7a38a3b12ed8
Kabir Hasan <kabir.webdev@gmail.com>
2022-01-19 01:05:23 +0600

    Update index.js

--- components/EditUser/index.js
+++ components/EditUser/index.js
@@ -9,50 +9,27 @@ const initialValue = {
   phone: "",
 };
 
-// export const getServerSideProps = async (context) => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
-//   const todo = await res.json();
 
-//   return {
-//     props: {
-//       todo,
-//     },
-//   };
-// };
 
 export async function getServerSideProps(context) {
   const { userid } = context.query;
   console.log(context);
-  // const response = await fetch(`http://localhost:5000/single-user/${userid}`);
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users/1`);
+  const response = await fetch(`http://localhost:5000/single-user/${userid}`);
   const info = await response.json();
   return {
     props: {
       info,
     },
   };
-
-  //   // const id = 1;
-  //   // const res = await fetch(`https://restcountries.eu/rest/v2/name/${id}`);
-  //   // const country = await res.json();
-  //   // console.log(`Fetched place: ${country.name}`);
-  //   // return { props: { country } };
-
-  //   // const { id } = context.params; // Use `context.params` to get dynamic params
-  //   //   const res = await fetch(`https://restcountries.com/v2/name/${id}`); // Using `restcountries.com` as `restcountries.eu` is no longer accessible
-  //   //   const countryList = await res.json();
-  //   //   const [country] = countryList; // Get first item in array returned from API
-
-  //   //   return { props: { country } };
 }
 
-const UserEdit = ({ todo }) => {
-  console.log("Props: ", todo);
+const UserEdit = ({ info }) => {
+  console.log("Props: ", info);
   const [book, setBook] = useState(initialValue);
 
-  // useEffect(() => {
-  //   setBook(info);
-  // }, [info]);
+  useEffect(() => {
+   setBook(info);
+ }, [info]);
 
   console.log("Books: ", book);
 
